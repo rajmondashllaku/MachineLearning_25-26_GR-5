d59f5d9126904c13417036f91ca14f89db397cf0
Fleta Mujaj <93597983+fletamujaj@users.noreply.github.com>
2026-03-19 15:25:00 +0100

    Implement data cleaning and outlier removal functions

--- FAZA-1_Pergatitja_e_modelit/data_cleaning.py
+++ FAZA-1_Pergatitja_e_modelit/data_cleaning.py
@@ -1,6 +1,57 @@
-def main():
-    # Your logic starts here
-    pass
+import pandas as pd
+
+def largo_outliers_iqr(df, kolonat, kufijte_mjedisor):
+    df_pastruar = df.copy()
+
+    for kolona in kolonat:
+        Q1 = df_pastruar[kolona].quantile(0.25)
+        Q3 = df_pastruar[kolona].quantile(0.75)
+        IQR = Q3 - Q1
+
+        kufiri_siperm_iqr = Q3 + (3.0 * IQR)
+
+        kufiri_minimal_i_lejuar = kufijte_mjedisor[kolona]
+        kufiri_siperm = max(kufiri_siperm_iqr, kufiri_minimal_i_lejuar)
+
+        kufiri_poshtem = 0
+
+        kushti = (df_pastruar[kolona] >= kufiri_poshtem) & (df_pastruar[kolona] <= kufiri_siperm)
+        rreshtat_para = len(df_pastruar)
+        df_pastruar = df_pastruar[kushti]
+        rreshtat_pas = len(df_pastruar)
+
+        print(
+            f" -> [{kolona}] U fshinë {rreshtat_para - rreshtat_pas} outliers (Kufiri i sipërm i aplikuar: {kufiri_siperm:.2f})")
+
+    return df_pastruar
+
+
+def pastro_te_dhenat(input_file, output_file):
+    print("--- FILLIMI I PASTRIMIT TË TË DHËNAVE ---")
+    df = pd.read_csv(input_file)
+    print(f"Madhësia e dataset-it PARA pastrimit: {len(df)} rreshta")
+
+    df_clean = df.dropna(subset=['pm25', 'pm10'])
+
+    df_clean = df_clean[(df_clean['pm25'] >= 0) & (df_clean['pm10'] >= 0)]
+
+    print("\nDuke aplikuar metodën IQR Hibride për detektimin e anomalive...")
+
+    limitet_reale = {
+        'pm25': 300.0,
+        'pm10': 500.0
+    }
+
+    df_clean = largo_outliers_iqr(df_clean, kolonat=['pm25', 'pm10'], kufijte_mjedisor=limitet_reale)
+
+    print(f"\nMadhësia e dataset-it PAS pastrimit: {len(df_clean)} rreshta")
+
+    df_clean.to_csv(output_file, index=False)
+    print(f"Të dhënat u pastruan dhe u ruajtën me sukses tek: {output_file}\n")
+
 
 if __name__ == "__main__":
-    main()
+    skedari_hyrje = '../Datasetet/processed_dataset/prishtina_integrated_data.csv'
+    skedari_dalje = '../Datasetet/processed_dataset/prishtina_cleaned_data.csv'
+
+    pastro_te_dhenat(skedari_hyrje, skedari_dalje)
