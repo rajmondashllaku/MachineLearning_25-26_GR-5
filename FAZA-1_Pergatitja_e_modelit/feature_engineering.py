d8dd741389decf5a573e8d302c2fa252a8ce2d57
endritavllasaliu16 <endrita.vllasaliu@student.uni-pr.edu>
2026-03-19 15:56:19 +0100

    feat: add feature engineering

--- FAZA-1_Pergatitja_e_modelit/feature_engineering.py
+++ FAZA-1_Pergatitja_e_modelit/feature_engineering.py
@@ -1,6 +1,26 @@
-def main():
-    # Your logic starts here
-    pass
+import pandas as pd
+
+
+def krijo_vecori_te_reja(input_file, output_file):
+    print("---FEATURE ENGINEERING---")
+    df = pd.read_csv(input_file)
+
+    df['time'] = pd.to_datetime(df['time'])
+
+    df['muaji'] = df['time'].dt.month
+    df['ora'] = df['time'].dt.hour
+
+    df = df.drop(columns=['time'])
+
+    df.to_csv(output_file, index=False)
+
+    print("Inxhinieria përfundoi! Dataset-i final është gati për trajnim.")
+    print(f"E ruajtur tek: {output_file}")
+    print(f"Kolonat finale: {list(df.columns)}\n")
+
 
 if __name__ == "__main__":
-    main()
+    skedari_hyrje = '../Datasetet/processed_dataset/prishtina_cleaned_data.csv'
+    skedari_dalje = '../Datasetet/processed_dataset/prishtina_ml_ready_data.csv'
+
+    krijo_vecori_te_reja(skedari_hyrje, skedari_dalje)
