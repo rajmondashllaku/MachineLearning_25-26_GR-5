09cdcfbe4d577fdcff3f45b2a6fb8978d26448c7
Rajmondë Shllaku <58296379+rajmondashllaku@users.noreply.github.com>
2026-05-12 19:34:46 +0200

    finetuned xgboost regressor

--- FAZA-3_Analiza_dhe_Evaluimi/retrain_xgboost_regressor.py
+++ FAZA-3_Analiza_dhe_Evaluimi/retrain_xgboost_regressor.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import xgboost as xgb
-from sklearn.model_selection import train_test_split  # E mbajmë për çdo rast, por s'do ta përdorim
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
